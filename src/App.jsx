623337c5cf152586246c791a12d1bc7def859139
wil <wil.fabri777@gmail.com>
2024-09-23 19:00:07 -0400

    iniciando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TablaInventario from './components/TablaInventario';
+
+function App() {
+  return (
+    <div className="App">
+      <TablaInventario />
+    </div>
+  );
+}
+
+export default App;
